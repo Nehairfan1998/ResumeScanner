568ab3776d8fef2eeb16cd475c7faa3a0f5c785a
neha irfan <93335205+Nehairfan1998@users.noreply.github.com>
2022-03-28 19:53:05 +0530

    + CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 import docx2txt
 
 app = Flask(__name__)
+CORS(app)
 @app.route("/")
 def resumeScanner():
     BestCandidate = "myResume1"
